d3c874f8bbcb0f0affffcda8b9d662697d490b8a
Lindsay Iveson <ivesonlindsay@gmail.com>
2019-11-13 23:51:12 -0500

    initial commit with created Nav and EnterText components

---
+++ src/components/Nav/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./style.css";
+
+function Nav() {
+    return (
+    <div className="nav">
+        <nav>
+            <ul>
+                <li>Anne Lister Code Generator</li>
+            </ul>
+        </nav>
+    </div>
+    );
+};
+
+export default Nav;
